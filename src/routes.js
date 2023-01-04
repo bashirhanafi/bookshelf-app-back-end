64d5067484facab2a8a9030fae1c89fa4d5ff3ae
Muhammad Bashir Hanafi <85800672+bashirhanafi@users.noreply.github.com>
2023-01-04 13:42:39 +0800

    Add files via upload

---
+++ src/routes.js
@@ -0,0 +1,35 @@
+const { addBooksHandler, getAllBooksHandler, getBookByIdHandler, deleteBookByIdHandler, updateBookByIdHandler } = require('./handler')
+
+const routes = [
+    {
+        method: 'POST',
+        path: '/books',
+        handler: addBooksHandler
+    },
+
+    {
+        method: 'GET',
+        path: '/books',
+        handler: getAllBooksHandler
+    },
+
+    {
+        method: 'GET',
+        path: '/books/{id}',
+        handler: getBookByIdHandler
+    },
+
+    {
+        method: 'PUT',
+        path: '/books/{id}',
+        handler: updateBookByIdHandler
+    },
+
+    {
+        method: 'DELETE',
+        path: '/books/{id}',
+        handler: deleteBookByIdHandler
+    },
+]
+
+module.exports = routes
